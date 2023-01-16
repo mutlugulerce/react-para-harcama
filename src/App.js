f3a9bc5dd90fa84a3875a36c72e8ef5b7a32dc63
Mutlu Gülerce <118522613+mutlugulerce@users.noreply.github.com>
2023-01-16 19:35:07 +0300

    css add, basket and basketItem create,header information

--- src/App.js
+++ src/App.js
@@ -1,41 +1,41 @@
-
-import { useEffect, useState } from "react";
-import Header from "./components/Header";
-import Product from "./components/Product";
+import './App.css';
+import {useState, useEffect} from 'react'
+import Header from './components/Header';
+import Product from './components/Product';
+import Basket from './components/Basket';
 import products from './products.json'
 
 function App() {
-   const [money,setMoney] = useState(50000000)
-   const [basket,setBasket] = useState([])
-   const [total,setTotal] =useState(0)
 
- useEffect(() => {
-  setTotal(
-	basket.reduce((acc,item) => {
-    return acc + (item.amount * (products.find(product => product.id === item.id)).price)
-	},0)
-  )
+	const [money, setMoney] = useState(1000000000)
+	const [basket, setBasket] = useState([])
+	const [total, setTotal] = useState(0)
 
+	const resetBasket = () => {
+		setBasket([])
+	}
 
-},[basket])
+	useEffect(() => {
+		setTotal(
+			basket.reduce((acc, item) => {
+				return acc + (item.amount * (products.find(product => product.id === item.id).price))
+			}, 0),
+		)
+	}, [basket])
 
-
-
-	return(
-<>
- 
- <Header money={money} />
-
-
- <div className="container products">
+	return (
+		<>
+			<Header total={total} money={money}/>
+			<div className="container products">
 				{products.map(product => (
-					<Product key={product.id}  product={product} basket={basket} setBasket={setBasket} money={money} total={total}/>
+					<Product key={product.id} total={total} money={money} basket={basket} setBasket={setBasket} product={product}/>
 				))}
 			</div>
-  
- 
+			{total > 0 && (
+				<Basket resetBasket={resetBasket} products={products} total={total} basket={basket} />
+			)}
+		</>
+	);
+}
 
-</>
-	)
-	}
 export default App;
